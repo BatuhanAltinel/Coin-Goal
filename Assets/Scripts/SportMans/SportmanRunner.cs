6134edd5ac214f138e4e745068f735c088e06693
DESKTOP-UFGH3NC\ELİF <batuhan.altinel@hotmail.com>
2023-02-28 16:24:49 +0300

    PowerUp implemented

--- Assets/Scripts/SportMans/SportmanRunner.cs
+++ Assets/Scripts/SportMans/SportmanRunner.cs
@@ -7,7 +7,8 @@ public class SportmanRunner : MonoBehaviour
     [SerializeField] Transform _rightWayPoint;
     [SerializeField] Transform _leftWayPoint;
     Transform targetWayPoint;
-
+     Vector3 _startPosition;
+    Quaternion _startRotation;
      [SerializeField] float _moveSpeed;
     float elapsedTime = 0;
 
@@ -20,12 +21,16 @@ public class SportmanRunner : MonoBehaviour
         EventManager.OnGoal += PlaySadAnim;
         EventManager.OnRestartLevel += PlayIdleAnim;
         EventManager.OnNextLevel += PlayIdleAnim;
+        EventManager.OnRestartLevel += OnRestart;
+        EventManager.OnNextLevel += OnRestart;
     }
     void Start()
     {
         _anim = GetComponent<Animator>();
         _anim.SetBool("IsGoal",false);
         targetWayPoint = _rightWayPoint;
+        _startPosition = transform.parent.position;
+        _startRotation = transform.parent.rotation;
     }
 
     void ChangeWayPoint()
@@ -57,15 +62,49 @@ public class SportmanRunner : MonoBehaviour
     {
         if(other.gameObject.TryGetComponent<Coin>(out Coin coin) && !GameManager.Instance.IsGoal)
         {
-            RandomShootVector();
-            PlayShootAnim();
+            if(coin.coinState == CoinStates.powerUp)
+            {
+                EventManager.OnCoinStateChanged.Invoke(CoinStates.normal);
+                OnCrushPowerUpCoin();
+            }else
+            {
+                RandomShootVector();
+                PlayShootAnim();
 
-            coin.MoveTo(_randomShootVector,_maxShootPower);
-            EventManager.OnKickBack.Invoke();
-            Invoke("PlayIdleAnim",0.2f);
+                coin.MoveTo(_randomShootVector,_maxShootPower);
+                EventManager.OnKickBack.Invoke();
+                Invoke("PlayIdleAnim",0.2f);
+            }
         }
     }
 
+    void OnRestart()
+    {
+        transform.parent.position = _startPosition;
+        transform.parent.rotation = _startRotation;
+    }
+
+    void OnCrushPowerUpCoin()
+    {
+        StartCoroutine(OnCrushPowerUpCoinRoutine());
+    }
+
+    IEnumerator OnCrushPowerUpCoinRoutine()
+    {
+        Vector3 targetPos = new Vector3(50,50,transform.position.z);
+        Quaternion targetRot = Quaternion.Euler(-155,180,-128);
+
+        while(Vector3.Distance(transform.position,targetPos) > 0.1f)
+        {
+            transform.parent.position = Vector3.Lerp(transform.parent.position,targetPos,0.1f);
+            transform.parent.rotation = Quaternion.Lerp(transform.parent.rotation,targetRot,0.1f);
+
+            yield return new WaitForEndOfFrame();
+        }
+        transform.parent.position = targetPos;
+        transform.parent.rotation = targetRot;
+        yield break;
+    }   
     void PlayIdleAnim()
     {
         _anim.SetBool("IsCoinCollision",false);
@@ -95,5 +134,7 @@ public class SportmanRunner : MonoBehaviour
         EventManager.OnGoal -= PlaySadAnim;
         EventManager.OnRestartLevel -= PlayIdleAnim;
         EventManager.OnNextLevel -= PlayIdleAnim;
+        EventManager.OnRestartLevel -= OnRestart;
+        EventManager.OnNextLevel -= OnRestart;
     }
 }
